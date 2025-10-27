9c256759029a7d2057fde7bce00d9a2dc711d427
timshowtimes <sultim99@gmail.com>
2025-10-28 00:41:19 +0500

    created skeleton of accounts-service, config-service, discovery-service, gateway-service

---
+++ accounts-service/src/test/java/kz/timshowtime/accountsservice/AccountsServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package kz.timshowtime.accountsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AccountsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ discovery-service/src/main/java/kz/timshowtime/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package kz.timshowtime.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
